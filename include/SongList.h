bd63547126191914aa3fc9a4559c8475f9ec743a
Anton Koetzler-Faust <antonkoetzler-faust@protonmail.com>
2022-01-19 13:40:24 -0700

    Starting to complete SongList.h impleist.h implementation

--- include/SongList.h
+++ include/SongList.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <wx/wx.h>
+#include <wx/dir.h>
 
 class SongList : public wxListBox
 {
@@ -8,4 +9,5 @@ public:
   SongList(wxFrame*);
 
 private:
+  wxDir* dir;
 };
--- src/SongList.cpp
+++ src/SongList.cpp
@@ -2,4 +2,8 @@
 
 SongList::SongList(wxFrame* parent) : wxListBox(parent, wxID_ANY, wxDefaultPosition, wxDefaultSize)
 {
+  dir = new wxDir("../songs/");
+
+  if (dir->IsOpened()) std::cout << "Working" << std::endl;
+  else std::cout << "Not Working" << std::endl;
 }
