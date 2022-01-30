d28f5d1cae5432f0748a99b8dd02dd26a7e4cf13
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2022-01-29 22:55:19 -0700

    Going to restart the implementation

--- src/MusicControls.cpp
+++
@@ -1,6 +0,0 @@
-#include "MusicControls.h"
-
-MusicControls::MusicControls(wxFrame* parent) : wxWindow(parent, wxID_ANY, wxDefaultPosition, wxSize(1000, 100))
-{
-  SetBackgroundColour(wxColour(255, 182, 193));
-}
--- src/MusicSelector.cpp
+++
@@ -1,7 +0,0 @@
-#include "MusicSelector.h"
-
-MusicSelector::MusicSelector(wxFrame* parent) : wxBoxSizer(wxHORIZONTAL)
-{
-  songlist = new SongList(parent);
-  Add(songlist, 1, wxEXPAND);
-}
