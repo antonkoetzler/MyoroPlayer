c63eecfd9af1f7d3bdd0b8b0f1dcf6e842d7cefb
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2022-05-07 18:16:53 -0600

    Forgor to remove all the songs I used for sampling lol

---
+++ build/CMakeFiles/MyoroPlayer.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/flqn/git/MyoroPlayer/src/App.cpp" "CMakeFiles/MyoroPlayer.dir/src/App.cpp.o" "gcc" "CMakeFiles/MyoroPlayer.dir/src/App.cpp.o.d"
+  "/home/flqn/git/MyoroPlayer/src/Controls.cpp" "CMakeFiles/MyoroPlayer.dir/src/Controls.cpp.o" "gcc" "CMakeFiles/MyoroPlayer.dir/src/Controls.cpp.o.d"
+  "/home/flqn/git/MyoroPlayer/src/Frame.cpp" "CMakeFiles/MyoroPlayer.dir/src/Frame.cpp.o" "gcc" "CMakeFiles/MyoroPlayer.dir/src/Frame.cpp.o.d"
+  "/home/flqn/git/MyoroPlayer/src/MenuBar.cpp" "CMakeFiles/MyoroPlayer.dir/src/MenuBar.cpp.o" "gcc" "CMakeFiles/MyoroPlayer.dir/src/MenuBar.cpp.o.d"
+  "/home/flqn/git/MyoroPlayer/src/SongList.cpp" "CMakeFiles/MyoroPlayer.dir/src/SongList.cpp.o" "gcc" "CMakeFiles/MyoroPlayer.dir/src/SongList.cpp.o.d"
+  "/home/flqn/git/MyoroPlayer/src/UpdateSlider.cpp" "CMakeFiles/MyoroPlayer.dir/src/UpdateSlider.cpp.o" "gcc" "CMakeFiles/MyoroPlayer.dir/src/UpdateSlider.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
