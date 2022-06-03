41e4817e853a2c0a229cfea9422e989f529ab92c
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2022-06-03 17:54:50 -0600

    I have using wxWidgets to make a cross platform music player and that is it

---
+++ include/Frame.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <wx/wx.h>
+
+class Frame : public wxFrame
+{
+ public:
+  Frame();
+
+ private:
+};
---
+++ src/Frame.cpp
@@ -0,0 +1,5 @@
+#include "Frame.h"
+
+Frame::Frame() : wxFrame(nullptr, wxID_ANY, "MyoroPlayer", wxDefaultPosition, wxSize(800, 600))
+{
+}
