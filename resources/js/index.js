f9ce43355e1789acc03f56d8eb727c94efe06638
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2022-06-02 19:28:58 -0600

    Gotta fix add playlist bug

--- resources/js/index.js
+++ resources/js/index.js
@@ -12,5 +12,36 @@ window.addEventListener("keydown", function()
     }
   }
 })
+window.addEventListener("click", function()
+{
+  // Clean .taskbarButton objects if mouse is clicked elsewhere
+  if (event.target.className != "taskbarButton") clean()
+})
 
 function exit() { Neutralino.app.exit() }
+
+// Disables: taskbar dropdowns
+function clean()
+{
+  // Taskbar objects
+  document.getElementById("file").style.display = "none"
+  document.getElementById("view").style.display = "none"
+  // Directory popup
+  document.getElementById("directoryPopup").style.display = "none"
+}
+
+// Either enables or disables a dropdown from the taskbar
+function toggleDropdown(id)
+{
+  let dropdown = document.getElementById(id)
+  let dropdownDisplay = window.getComputedStyle(dropdown).display
+  clean()
+  if (dropdownDisplay == "none") dropdown.style.display = "flex"
+  else                           dropdown.style.display = "none"
+}
+
+// Enables #directoryPopup to add a new playlist
+function toggleDirectoryPopup()
+{
+  document.getElementById("directoryPopup").style.display = "flex"
+}
