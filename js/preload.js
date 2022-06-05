7749b74c721bbb0e9fff5d9678108b04fdfdbf21
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2022-06-05 01:00:21 -0600

    Adding playlists IS WORKING, now I must do the .savedPlaylists part

--- js/preload.js
+++ js/preload.js
@@ -1,17 +1,20 @@
 const { contextBridge, ipcRenderer } = require("electron")
 
+
+// Adds the directory selected in ipcMain.on("addPlaylist...
 ipcRenderer.on("getPlaylist", (event, dir) => {
   // Turning the dir into a string
   let directory = new String(dir)
 
   // Getting the name of the directory
   let name = null
-  if (directory == "/") name = directory
+  if (directory == "/" || directory.length == 3 && directory.substr(1) == ":\\")
+    name = directory
   else
   {
     for (var i = (directory.length - 1); i >= 0; i--)
     {
-      if (directory[i] == '/')
+      if (directory[i] == '/' || directory[i] == '\\')
       {
         name = directory.substr(i + 1)
         break
@@ -26,10 +29,11 @@ ipcRenderer.on("getPlaylist", (event, dir) => {
   let button = document.createElement("button")
   button.innerHTML = name
   button.className = "taskbarButton sidebarButton"
-  button.setAttribute("onClick", "openPlaylist('" + directory + "')")
+  // replaceAll is used to make sure JS reads backslashes
+  button.setAttribute("onClick", "openPlaylist('" + directory.replaceAll('\\', '\\\\') + "')")
   document.getElementById("sidebar").appendChild(button)
 
-  // Disabling taskbar menus
+  // Since we don't have access to cleanDropdowns, we disable dropdowns manually
   document.getElementById("file").style.display = "none"
   document.getElementById("view").style.display = "none"
 })
